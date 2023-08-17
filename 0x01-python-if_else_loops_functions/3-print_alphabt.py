31ea18b21905ade1cd972c3b8573b65e7fb76ffe
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-17 07:46:08 -0700

    except 'q' or 'e' error

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for ASCII_value in range(ord('a'), ord('z')):
+for ASCII_value in range(ord('a'), ord('z') + 1):
     if ASCII_value != ord('q') and ASCII_value != ord('e'):
         print("{}".format(chr(ASCII_value)), end="")
