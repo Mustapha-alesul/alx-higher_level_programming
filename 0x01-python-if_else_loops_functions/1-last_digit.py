f8ceaa20af554b92081ec03fcd5846dd72df5def
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-17 00:37:28 -0700

    correcting space errors

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,7 @@ import random
 number = random.randint(-10000, 10000)
 last_num = abs(number) % 10
 
-if lasy_num > 5:
+if last_num > 5:
     print("Last digit of " + str(number) + " is " + str(last_num) +
           " and is greater than 5")
 elif last_num == 0:
