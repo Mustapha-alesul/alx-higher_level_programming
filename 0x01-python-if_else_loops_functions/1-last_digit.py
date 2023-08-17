62b561c6b92b9284de2aadc885ead665586a4ca2
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-17 05:22:24 -0700

    negative sign debugg

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,7 +4,6 @@ number = random.randint(-10000, 10000)
 l_digit = abs(number) % 10
 if number < 0:
     l_digit = -l_digit
-    print("Last digit of {} is {} and is".format(number, l_digit), end = "")
 if l_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, l_digit), end = "")
 elif l_digit == 0:
