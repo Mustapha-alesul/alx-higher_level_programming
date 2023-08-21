2a49c0043baa6c37ba52eebb65ca0da916d42ab9
Mustapha-alesul <mustapha.bdsh@gmail.com>
2023-08-21 08:50:53 -0700

    ERROR1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    argv = sys.argv[1:]
-    argm = len(argv)
-
-    print("{} argument{}{}".format(argm, "" 
-        if argm == 1 else "s", ":" if argm > 0 else "."))
-
-    for i, ar in enumerate(argv, start = 1):
-        print("{}: {}".format(i, ar))
+    import sys
+    le = len(sys.argv ) - 1
+    if le == 0:
+        print("0 arguments.")
+    elif le == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(le))
+    for n in range(le):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
